68bc7e5fe8e1a8ddbadedff308cd130b150d97f3
“cmferz” <cristian@uma.es>
2025-11-17 11:35:04 +0100

    Feat: first commit! :)

---
+++ ecg.py
@@ -0,0 +1,12 @@
+import neurokit2 as nk
+
+# Generate 15 seconds of ECG signal (recorded at 250 samples/second)
+ecg = nk.ecg_simulate(duration=15, sampling_rate=250, heart_rate=70)
+
+# Process it
+signals, info = nk.ecg_process(ecg, sampling_rate=250)
+
+# Visualise the processing
+nk.ecg_plot(signals, info)
+# Print the info dictionary to see extracted features
+print(info)
